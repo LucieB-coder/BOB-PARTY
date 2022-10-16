790e12d656e358e687751516ae330dd1e04a63cd
alguilhot <alban.guilhot@etu.uca.fr>
2022-10-16 11:46:45 +0200

    ADD: Ajout du fichier style du skinComponent

---
+++ bob_party/src/components/style/Skin.style.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    icon: {
+        width: 50,
+        height: 50,
+        borderRadius: 50,
+    },
+    imageWrapper: {
+        height: 135,
+        width: '40%',
+        flexDirection: "column",
+        margin: 10,
+    },
+    imageSkin : {
+      borderRadius: 15,
+      width: '100%',
+      flex:1
+    },
+    nomSkin :{
+      textAlign: 'center',
+      fontSize: 25,
+      fontFamily: 'Helvetica',
+      fontWeight: 'bold',
+      color: 'white',
+    },
+});
