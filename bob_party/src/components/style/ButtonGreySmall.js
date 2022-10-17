625a91d262fbb2088df9671fc04e1c08a300b533
alguilhot <alban.guilhot@etu.uca.fr>
2022-10-17 17:52:33 +0200

    ADD: ajout de case sur ButtonGreySmall

---
+++ bob_party/src/components/style/ButtonGreySmall.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({  
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+    marginTop: 5,
+    borderRadius: 10,
+    backgroundColor: '#2D2C33',
+    paddingHorizontal: 10,
+  },
+  buttonProfile: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+    marginTop: 5,
+    borderRadius: 10,
+    backgroundColor: '#2D2C33',
+    marginHorizontal: 10,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
