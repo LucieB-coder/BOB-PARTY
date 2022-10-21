2753d26cc37e2e6ed06b16486e7862756fd5a4e7
alguilhot <alban.guilhot@etu.uca.fr>
2022-10-21 18:24:15 +0200

    ADD: Ajout de fichiers de constantes pour tester l'app

---
+++ bob_party/src/constNat.ts
@@ -0,0 +1,11 @@
+
+const tabNat = [
+    { label: "France", value: "Français" },
+    { label: "Allemagne", value: "Allemand" },
+    { label: "Espagne", value: "Espagnol" },
+    { label: "Royaume-Uni", value: "Anglais" },
+    { label: "États-Unis", value: "Américain" },
+    { label: "Italie", value: "Italien" },
+]
+
+export default tabNat;
