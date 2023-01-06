a1a025c5e3254be00ac64f15326af12bcfa7d6c0
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-01-06 16:16:15 +0100

    Mise à jour de 'api-rest/index.php'

--- api-rest/index.php
+++ api-rest/index.php
@@ -19,11 +19,9 @@
     // ------
     //$ini_array= parse_ini_file("config.ini");
     //
-    $dsn = "mysql:host=". getenv("DB_SERVER").";dbname=". getenv("MYSQL_DATABASE");
+    $dsn = "mysql:host=". getenv("DB_SERVER").";port=8888;dbname=". getenv("MYSQL_DATABASE");
     $username = "root";
     $password = getenv("MYSQL_ROOT_PASSWORD");
-
-    echo $password;
 
     // Initializing Database
     try{
