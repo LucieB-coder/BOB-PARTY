777e9e947737bcdd098eece0c38319bdf318a2af
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-01-06 01:04:36 +0100

    Mise à jour de 'api-rest/index.php'

--- api-rest/index.php
+++ api-rest/index.php
@@ -7,7 +7,7 @@
     ///
 
     include ('dbConnection.php');
-    include ('userGateway.php');
+    include ('gateways/userGateway.php');
     include ('gateways/matchGateway.php');
     include ('gateways/conversationGateway.php');
     include ('gateways/gameGateway.php');
