be58e9f0338a4b9be16b4e5dfac36b679145cd94
Cédric BOUHOURS <cedric.bouhours@uca.fr>
2023-01-09 21:22:38 +0100

    Mise à jour de 'api-rest/index.php'

--- api-rest/index.php
+++ api-rest/index.php
@@ -2,7 +2,7 @@
 
 
 
-    $conn = new mysqli($"BOB_PARTEAM-mysql",getenv("MYSQL_USER_TOM"),getenv("MYSQL_PASSWORD_TOM"),getenv("MYSQL_DATABASE"));
+    $conn = new mysqli("BOB_PARTEAM-mysql",getenv("MYSQL_USER_TOM"),getenv("MYSQL_PASSWORD_TOM"),getenv("MYSQL_DATABASE"));
     exit;
 
     header('Access-Control-Allow-Origin: *');
