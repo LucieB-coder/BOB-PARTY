03106aba39c3e13f1f12b76723dacf54a29efa9f
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-01-10 00:26:22 +0100

    Mise à jour de 'api-rest/index.php'

--- api-rest/index.php
+++ api-rest/index.php
@@ -12,8 +12,8 @@
 
 
 
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST,GET,DELETE,PUT');
+    //header('Access-Control-Allow-Origin: *');
+    //header('Access-Control-Allow-Methods: POST,GET,DELETE,PUT');
     /// Good to know :
     /// All the values must be cleaned on the application side before using this API
     ///
