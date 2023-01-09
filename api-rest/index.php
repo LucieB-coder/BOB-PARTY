112888506c3aaef52090218786dbf46107d13a4a
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-01-10 00:00:21 +0100

    Mise à jour de 'api-rest/index.php'

--- api-rest/index.php
+++ api-rest/index.php
@@ -4,13 +4,11 @@
         $conn->query("CREATE TABLE coucou (`COUCOU_ID` int(10) unsigned NOT NULL,`COUCOU_TEXT` char(10) NOT NULL);");
         $conn->query("INSERT INTO `coucou` VALUES(1,'TOTO');");
         $res = $conn->query("SELECT * FROM coucou");
-        $res = $conn->query("SELECT * FROM T_H_SKIN_SKI");
-        echo $res;
-        //while ($une_valeur = $res->fetch_array()) {
-            //echo $une_valeur["COUCOU_ID"]." ".$une_valeur["COUCOU_TEXT"];
-            //echo json_encode($une_valeur);
-        //}
-    exit;
+
+        while ($une_valeur = $res->fetch_array()) {
+            echo $une_valeur["COUCOU_ID"]." ".$une_valeur["COUCOU_TEXT"];
+        }
+    //exit;
 
 
 
