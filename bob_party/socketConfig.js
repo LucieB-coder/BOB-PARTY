6fe0efbce2f805e64305865ab9980a305749321c
Thomas Chazot <tchazot63@gmail.com>
2023-01-11 09:31:34 +0100

    Vérification nombre de joueurs pour match

--- bob_party/socketConfig.js
+++ bob_party/socketConfig.js
@@ -1,4 +1,4 @@
 const { io } = require("socket.io-client");
 
 
-export const socket = io("http://codefirst.iut.uca.fr/containers/BOB_PARTEAM-server-bobParty:3000");
+export const socket = io("https://codefirst.iut.uca.fr/containers/BOB_PARTEAM-server-bobParty/server:3000");
