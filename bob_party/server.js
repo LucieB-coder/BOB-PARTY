44de84190bc9ccf29deb0443ad92f82988ba6cbf
Thomas Chazot <tchazot63@gmail.com>
2023-01-11 11:16:42 +0100

    Test pour la connection au serveur

--- bob_party/server.js
+++ bob_party/server.js
@@ -2,11 +2,22 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
+const test = require('is-docker');
+
+
 
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+if (test.isDocker()) {
+	console.log('Running inside a Docker container');
+}
+
+app.get('/', function (req, res) {
+  res.sendFile('it should work');
+});
 
 io.on('connection', (socket) => {
   console.log(socket.id);  
