8695c9b8c6115751a34e0b717a34d5176f20ad8d
Thomas CHAZOT <thomas.chazot2@etu.uca.fr>
2023-01-11 09:34:25 +0100

    Mise à jour de 'bob_party/server.js'

--- bob_party/server.js
+++ bob_party/server.js
@@ -6,7 +6,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/server', function (req, res) {
+io.get('/server', function (req, res) {
     res.send('hello');
 })
 
