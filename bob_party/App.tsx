63d299078944d4811fee1861a0edf44c9822b073
aguilhot <Alban.GUILHOT@etu.uca.fr>
2023-01-19 10:58:08 +0100

    Resolution Warnings

--- bob_party/App.tsx
+++ bob_party/App.tsx
@@ -6,6 +6,9 @@ import { useFonts } from 'expo-font';
 import TicTacToeOnline from './src/Games/Tic-Tac-Toe/tic_tac_toe_online';
 import BlackJack from './src/Games/BlackJack/blackJack';
 
+// RN >= 0.63
+import { LogBox } from 'react-native';
+
 
 export default function App() {
 
@@ -18,6 +21,7 @@ export default function App() {
     return null;
   }
 
+  LogBox.ignoreLogs(['Warning:...', 'Require', 'Constants']);
 
   return (
 
